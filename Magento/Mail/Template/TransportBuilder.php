dede80609d9ec28c88e1f8bf239187e0fa3c296e
Timur Safuanov <timhannn@gmail.com>
2019-02-18 13:18:48 +0300

    [rename]Refactor comments in Magento directory

--- Magento/Mail/Template/TransportBuilder.php
+++ Magento/Mail/Template/TransportBuilder.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Mageinn
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageinn.com license that is
+ * available through the world-wide-web at this URL:
+ * https://mageinn.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ */
 namespace Mageinn\Dropship\Magento\Mail\Template;
 
 /**
@@ -8,8 +23,6 @@ namespace Mageinn\Dropship\Magento\Mail\Template;
 class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
 {
     /**
-     * Adds an attachment to the email
-     *
      * @param $body
      * @param null $filename
      * @param string $mimeType
